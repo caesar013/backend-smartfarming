8e6793c5a8da5017e6e1ebb8b75461b4bca0a7e5
cαεsαɾ <91421259+caesar013@users.noreply.github.com>
2024-12-04 20:37:15 +0700

    [add] migration: tables to store hourly and daily average of dht and npk data

---
+++ database/migrations/1733312919810_dht_average_dailies.ts
@@ -0,0 +1,21 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'dht_average_dailies'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('temperature')
+      table.integer('humidity')
+      table.integer('luminosity')
+      table.bigInteger('sensor_id').unsigned();
+      table.foreign('sensor_id').references('id').inTable('sensors');
+      table.timestamp('created_at').defaultTo(this.now())
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
