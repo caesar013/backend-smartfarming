a03be613332083c4ae542489a6d2aa567d9c0213
caesar <fikrilha099@gmail.com>
2024-12-17 20:44:28 +0700

    [update] add new table into enum

--- app/Enums/TABLE.ts
+++ app/Enums/TABLE.ts
@@ -3,4 +3,10 @@ export enum TABLE {
   DHT = 'dhts',
   NPK_1 = 'npks',
   NPK_2 = 'npks',
+  DHT_DAILY = 'dht_average_dailies',
+  NPK_1_DAILY = 'npk_average_dailies',
+  NPK_2_DAILY = 'npk_average_dailies',
+  DHT_HOURLY = 'dht_average_hourlies',
+  NPK_1_HOURLY = 'npk_average_hourlies',
+  NPK_2_HOURLY = 'npk_average_hourlies',
 }
