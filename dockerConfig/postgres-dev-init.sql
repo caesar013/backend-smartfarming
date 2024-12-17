b06eaed7c4d85ec44a271fb2010ce412bd81e844
caesar <fikrilha099@gmail.com>
2024-12-17 14:17:50 +0700

    [update] give privilege to user

--- dockerConfig/postgres-dev-init.sql
+++ dockerConfig/postgres-dev-init.sql
@@ -1,3 +1,5 @@
 CREATE USER sf_adonis WITH ENCRYPTED PASSWORD 'pass_sf_adonis';
 
 ALTER DATABASE sf_db OWNER TO sf_adonis;
+ALTER SCHEMA "public" OWNER TO sf_adonis;
+ALTER SCHEMA "user" OWNER TO sf_adonis;
