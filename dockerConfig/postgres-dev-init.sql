84ba5c8c545975ddfec05a462717c40fdc73e04e
caesar <fikrilha099@gmail.com>
2024-12-19 14:15:06 +0700

    [update] alter table on user schema owner to our app

--- dockerConfig/postgres-dev-init.sql
+++ dockerConfig/postgres-dev-init.sql
@@ -3,3 +3,7 @@ CREATE USER sf_adonis WITH ENCRYPTED PASSWORD 'pass_sf_adonis';
 ALTER DATABASE sf_db OWNER TO sf_adonis;
 ALTER SCHEMA "public" OWNER TO sf_adonis;
 ALTER SCHEMA "user" OWNER TO sf_adonis;
+
+ALTER TABLE "user"."account" OWNER TO sf_adonis;
+ALTER TABLE "user"."role" OWNER TO sf_adonis;
+ALTER TABLE "user"."api_tokens" OWNER TO sf_adonis;
