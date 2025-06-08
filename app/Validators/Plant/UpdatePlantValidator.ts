86d47be2a40707e7b4baf4f62c1a360836879d6b
caesar <fikrilha099@gmail.com>
2025-06-08 21:15:12 +0700

    [add] update validator

---
+++ app/Validators/Plant/UpdatePlantValidator.ts
@@ -0,0 +1,25 @@
+import { schema, validator, rules } from '@ioc:Adonis/Core/Validator'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class UpdatePlantValidator {
+  constructor(protected ctx: HttpContextContract) {
+  }
+
+  public reporter = validator.reporters.api
+
+  public schema = schema.create({
+    // your validation rules
+    name: schema.string.optional({}, [
+      rules.minLength(1),
+      rules.maxLength(100),
+    ]),
+    scientific_name: schema.string.optional({}, [
+      rules.minLength(1),
+      rules.maxLength(100)
+    ]),
+    description: schema.string.nullableAndOptional({}, [
+      rules.nullable(),
+      rules.maxLength(255)
+    ]),
+  })
+}
