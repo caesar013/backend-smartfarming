8568310991aca5378667d8e4cbc2ae209c1125c3
caesar <fikrilha099@gmail.com>
2025-06-15 00:20:34 +0700

    [add] actuators

--- app/Models/Actuator/Actuator.ts
+++ app/Models/Actuator/Actuator.ts
@@ -10,23 +10,23 @@ export default class Actuator extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
-  @column()
+  @column({ serializeAs: 'actuatorTypeId' })
   public actuatorTypeId: number
 
-  @column()
+  @column({ serializeAs: 'bedLocationId' })
   public bedLocationId: number | null
 
   @column()
   public name: string
 
-  @column.dateTime({ autoCreate: true })
-  public created_at: DateTime
+  @column.dateTime({ autoCreate: true, serializeAs: 'createdAt' })
+  public createdAt: DateTime
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updated_at: DateTime | null
+  @column.dateTime({ autoCreate: true, autoUpdate: true, serializeAs: 'updatedAt' })
+  public updatedAt: DateTime | null
 
-  @column.dateTime()
-  public deleted_at: DateTime | null
+  @column.dateTime({ serializeAs: 'deletedAt' })
+  public deletedAt: DateTime | null
 
   static get table() {
     return "public.actuators" // table name
---
+++ app/Validators/Actuator/UpdateActuatorValidator.ts
@@ -0,0 +1,23 @@
+import { schema, validator, rules } from '@ioc:Adonis/Core/Validator'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class UpdateActuatorValidator {
+  constructor(protected ctx: HttpContextContract) {
+  }
+
+  public reporter = validator.reporters.api
+
+  public schema = schema.create({
+    // your validation rules
+    actuatorTypeId: schema.number.nullable([
+      rules.exists({ table: 'actuator_types', column: 'id' })
+    ]),
+    bedLocationId: schema.number.nullableAndOptional([
+      rules.exists({ table: 'bed_locations', column: 'id' }),
+    ]),
+    name: schema.string.optional({ trim: true }, [
+      rules.maxLength(255),
+      rules.minLength(1),
+    ]),
+  })
+}
