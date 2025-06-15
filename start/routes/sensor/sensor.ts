08f954072416ead2ba543ef09356b39d163efac5
caesar <fikrilha099@gmail.com>
2025-06-15 12:45:41 +0700

    [update] route back to the original controller

--- start/routes/sensor/sensor.ts
+++ start/routes/sensor/sensor.ts
@@ -1,8 +1,8 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(function () {
-  Route.get('/getData', 'SensorReading/SensorReadingController.getData').as('sensor.getData')
-  Route.get('/getLatest', 'SensorReading/SensorReadingController.getLatest').as('sensor.getLatest')
+  Route.get('/getData', 'Sensor/SensorController.getData').as('sensor.getData')
+  Route.get('/getLatest', 'Sensor/SensorController.getLatest').as('sensor.getLatest')
   Route.delete('/', 'Sensor/SensorController.destroyAll').as('sensor.destroyAll')
 }).prefix('sensor')
 Route.resource('/sensors', 'Sensor/SensorController').apiOnly()
