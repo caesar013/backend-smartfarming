5e4910ba44a27eff1d7027110e9b5d915f108c71
caesar <fikrilha099@gmail.com>
2025-06-19 23:00:55 +0700

    [update] sensor readings route

--- start/routes/sensorreading/sensor-reading.ts
+++ start/routes/sensorreading/sensor-reading.ts
@@ -4,5 +4,5 @@ Route.group(function () {
   Route.delete('/', 'SensorReading/SensorReadingController.destroyAll').as('sensor-reading.destroyAll')
   Route.get('/latest', 'SensorReading/SensorReadingController.getLatest').as('sensor-reading.getLatest')
   Route.get('/search', 'SensorReading/SensorReadingController.search').as('sensor-reading.search').middleware(['normalizeQueryFilter'])
-}).prefix('sensor-reading')
-Route.resource('sensor-reading', 'SensorReading/SensorReadingController').apiOnly()
+}).prefix('sensor-readings')
+Route.resource('sensor-readings', 'SensorReading/SensorReadingController').apiOnly()
