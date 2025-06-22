30d50ff1727a6228c05eec74b6218454f9510a70
caesar <fikrilha099@gmail.com>
2025-06-22 22:43:57 +0700

    [update] use slug in url

--- start/routes/actuator/actuators.ts
+++ start/routes/actuator/actuators.ts
@@ -2,6 +2,6 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(function () {
   Route.delete('/', 'Actuator/ActuatorController.destroyAll').as('actuators.destroyAll')
-  Route.post('/:id/control', 'Actuator/ActuatorController.control').as('actuators.control')
+  Route.post('/:slug/control', 'Actuator/ActuatorController.control').as('actuators.control')
 }).prefix('actuators')
 Route.resource('actuators', 'Actuator/ActuatorController').apiOnly()
