3f9b17b7a7df38cdfb1dc16ab41ff7bb0b6c2017
caesar <fikrilha099@gmail.com>
2025-06-26 11:47:49 +0700

    fix(auth): update unique validation rule for postgres compatibility

--- app/Controllers/Http/Auth/AuthController.ts
+++ app/Controllers/Http/Auth/AuthController.ts
@@ -23,13 +23,15 @@ export default class AuthController {
     try {
       const registerValidation = schema.create({
         username: schema.string({}, [
-          rules.maxLength(25)
+          rules.maxLength(25),
+          rules.unique({ table: 'user.account', column: 'username' }),
         ]),
         password: schema.string({}, [
           rules.minLength(8)
         ]),
         email: schema.string({}, [
-          rules.email()
+          rules.email(),
+          rules.unique({ table: 'user.account', column: 'email' }),
         ]),
         fullname: schema.string(),
       });
