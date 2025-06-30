7ecbe457f609d2593f1813487065fa0498f92ac0
caesar <fikrilha099@gmail.com>
2025-06-30 13:53:11 +0700

    refactor(sensor): remove unsused NPK and DHT models and enums

--- app/Enums/DHT.ts
+++
@@ -1,7 +0,0 @@
-export enum DHT {
-  // key = database column, value = incoming data
-  TEMPERATURE = 'viciTemperature',
-  HUMIDITY = 'viciHumidity',
-  LUMINOSITY = 'viciLuminosity',
-  READ_AT = 'time',
-}
